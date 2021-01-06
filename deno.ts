991df0b6ef214c25e75b42e256bc3aa603548933
Bob Bass <bob@bbass.co>
2021-01-05 20:08:12 -0500

    first project

---
+++ deno.ts
@@ -0,0 +1,4 @@
+const features: string[] = ['secure', 'modular', "typescript compatible"]
+const runtime: string = "Deno"
+const welcome = `Welcome to ${runtime}, the ${features.toString()} runtime!`
+console.log(welcome)
