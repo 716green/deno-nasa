a88f4603f1896a742cfd835829b649b07485b451
Bob Bass <bob@bbass.co>
2021-01-05 20:13:35 -0500

    added args to first ts function

--- deno.ts
+++ deno.ts
@@ -1,4 +1,13 @@
+const food = Deno.args[0]
+
 const features: string[] = ['secure', 'modular', "typescript compatible"]
 const runtime: string = "Deno"
 const welcome = `Welcome to ${runtime}, the ${features.toString()} runtime!`
 console.log(welcome)
+
+
+if (food) {
+  console.log('🦕...Deno is born!')
+} else {
+  console.log('🥚 ...the egg needs more love')
+}
